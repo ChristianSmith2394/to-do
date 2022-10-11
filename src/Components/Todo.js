f23f199dbd41cbc11ced379040b1c4a02abc55eb
Christian <christiansmith2394@gmail.com>
2022-10-11 19:11:59 -0400

    fixed completed item styling

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -18,7 +18,7 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
 
   return todos.map((todo, index) => (
     <div
-      className={todo.isComplete ? "todo-row-complete" : "todo-row"}
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
       key={index}
     >
       <div
